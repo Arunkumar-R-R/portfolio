e79c3cf35851f4810dfdb9a35f0059ce15954ee5
arunkumar-r-r <rrarunkumardgct@gmail.com>
2022-10-03 20:35:29 +0530

    updated chip component

--- components/Chip/Chip.js
+++ components/Chip/Chip.js
@@ -1,8 +1,8 @@
 export function Chip(props) {
   const { children, icon } = props;
   return (
-    <div className="shadow-sm py-[4px] px-[10px] mt-[16px] mr-4 rounded-full flex justify-center items-center text-tiny font-bold text-custom_black-700 border border-custom_black-500">
-      {icon ? <div className="mr-[8px]">{icon}</div> : ""}
+    <div className="shadow-sm py-[6px] px-[12px] mt-[16px] mr-4 rounded-full flex justify-center items-center text-tiny font-bold text-custom_black-700 border border-custom_black-500">
+      {icon ? <div className="mr-[6px]">{icon}</div> : ""}
       {children}
     </div>
   );
