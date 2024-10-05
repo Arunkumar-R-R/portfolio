650f870b53aa1e73eff92d356f237baee691bfd3
arunkumar-r-r <rrarunkumardgct@gmail.com>
2024-10-05 21:05:26 +0530

    changes i done before

---
+++ src/components/loadingScreen/LoadingScreen.jsx
@@ -0,0 +1,10 @@
+import { Fragment } from "react";
+import vanakkam from "./../../public/svg/vanakkam.svg?raw";
+
+export default function LoadingScreen() {
+    return (
+        <>
+        {{ vanakkam}}
+        </>
+    )
+}
